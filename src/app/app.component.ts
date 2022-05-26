560552f3133b1a209c70e3e4e95c9c79e7cbdd2f
mikseros <mmadynski@gmail.com>
2022-05-26 15:44:34 +0200

    Edit/update employee; sending to the backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit {
   public employees!: Employee[];
+  public editEmployee: Employee;
 
   constructor(private employeeService: EmployeeService){}
 
@@ -42,6 +43,18 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
   public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container')!;
 
@@ -53,6 +66,7 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#addEmployeeModal');
     }
     if (mode === 'edit') {
+      this.editEmployee = employee;
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (mode === 'delete') {
